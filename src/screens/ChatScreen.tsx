f313854841d1189e24bb41afab487857b8eed76d
Motcrash <Motcrash@gmail.com>
2023-07-02 23:30:01 -0600

    Title style added

--- src/screens/ChatScreen.tsx
+++ src/screens/ChatScreen.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import { Text, View } from 'react-native'
+import { styles } from '../theme/appTheme';
 
 export const ChatScreen = () => {
   return (
-    <View>
-        <Text>Chat</Text>
+    <View style= {{
+      ...styles.globalMargin,
+    }}>
+        <Text style = {{
+            ...styles.title,
+            color: '#62462a' 
+        }}>Chat</Text>
     </View>
   )
 }
