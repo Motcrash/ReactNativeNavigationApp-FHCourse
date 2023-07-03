6d0cbb470718d9d8310dda93c41775d395214126
Motcrash <Motcrash@gmail.com>
2023-07-02 22:26:16 -0600

    TouchableIcon created

---
+++ src/Components/TouchableIcon.tsx
@@ -0,0 +1,27 @@
+import React, { useContext } from 'react'
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { AuthContext } from '../Context/AuthContext';
+
+interface iconsProps {
+    iconName: string,
+
+}
+
+export const TouchableIcon = ( { iconName }: iconsProps ) => {
+
+    const { changeFavouriteIcon } = useContext( AuthContext )
+
+  return (
+    <TouchableOpacity
+        onPress={ () => changeFavouriteIcon( iconName ) }
+    >
+        <Icon 
+            name= { iconName }
+            size= { 50 }
+            color="#62462a"
+        />
+
+    </TouchableOpacity>
+  )
+}
